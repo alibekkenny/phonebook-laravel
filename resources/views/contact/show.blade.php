fda75e34d66e608a39cb47f5a922559c086d8316
Alibek <alibek.keneskhanov@gmail.com>
2024-03-11 18:48:32 +0600

    initial commit

---
+++ resources/views/contact/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('title', 'Contacts page')
+
+@section('content')
+    <div>Hello world, this is contacts page!</div>
+
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+
+{{--@section('title')--}}
+
+@section('content')
+
+    <img src="https://cdn.iconscout.com/icon/free/png-256/free-phone-book-1404933-1187580.png"
+         class="mx-auto d-block img-fluid"
+         alt="Phonebook Image">
+    <!--<h1 class="text-center mt-5"> Here will be some information about phone book!</h1>-->
+    <!--<h1 class="text-center mt-2">But now you can go to contacts list</h1>-->
+    <div class="text-center">
+        <p class="h2 mt-5">Here should be some info about phonebook!</p>
+        <footer class="footer">At this moment you can visit your  <a href="/1">contacts list</a>
+        </footer>
+    </div>
+
+@endsection
